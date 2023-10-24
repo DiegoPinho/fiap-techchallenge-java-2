51ba5a4da77190bd840a9ebf2e9fc6b8598f7932
Diego Martins de Pinho <diegomartinsdepinho@msn.com>
2023-10-24 10:25:40 -0300

    first commit

---
+++ src/test/java/com/fiap/techchallenge/diegopinho/parkingmeter/ParkingmeterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.techchallenge.diegopinho.parkingmeter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingmeterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
